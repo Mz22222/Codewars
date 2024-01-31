942c7e4174df9880fc372384e15e87fee4b834cf
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-31 21:04:57 +0300

    add 8kyu:reduce_but_grow

---
+++ 8_kyu/Reduce_but_Grow/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Учитывая непустой массив целых чисел, верните результат умножения значений по порядку. 
+Пример:
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 */
+
+public class Kata
+{
+  public static int Grow(int[] x)
+  {
+    int product = 1;
+    for (int i = 0; i < x.Length; i++)
+    {
+      product *= x[i];
+    }
+    return product;
+  }
+}
