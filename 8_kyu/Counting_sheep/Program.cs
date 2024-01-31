34102e5647bbd23e079a8fd4e811b4bb2e017709
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-02-01 00:57:00 +0300

    add 8kyu:Counting_Sheeps

---
+++ 8_kyu/Counting_sheep/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Рассмотрим массив / список овец, в котором некоторые овцы могут отсутствовать на своем месте. Нам нужна функция, 
+которая подсчитывает количество овец, присутствующих в массиве (true означает присутствует).
+Например,
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]  */
+
+  public static class Kata
+{
+  public static int CountSheeps(bool[] sheeps)
+  {
+    int count = 0;
+    for (int i = 0; i < sheeps.Length; i++)
+    {
+      if (sheeps[i])
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+}
