9ee53fc9fe58898e7a6980bb3e04a106e54910e5
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-02-03 21:14:11 +0300

    add 8_kyu:Return

---
+++ 8_kyu/Fundamentals_Return/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Создайте несколько функций, которые будут возвращать сумму, разность, модуль, произведение, частное и показатель степени соответственно.
+Пожалуйста, используйте следующие названия функций:
+* дополнение = Добавить
+* умножать = Умножать
+* разделение = Divide
+* модуль = Mod
+* экспоненциальный = Показатель степени
+* вычитание = Подзаголовок
+Примечание: Все функции могут возвращать значение int, и все они будут получать 2 целых числа.
+Примечание: Все математические операции будут: a (операция) b */
+
+using System;
+public static class Kata
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+    public static int Subt(int a, int b)
+    {
+        return a - b;
+    }
+    public static int Exponent(int a, int b)
+    {
+        return (int)Math.Pow(a, b);
+    }
+    public static int Mod(int a, int b)
+    {
+        return a % b;
+    }
+}
